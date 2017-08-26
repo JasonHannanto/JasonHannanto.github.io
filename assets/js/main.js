d8bd7c291ca552ab987c92923071457865046a72
Jason Hannanto <jason.hannanto@gmail.com>
2017-08-26 01:11:04 -0700

    Large Overhaul - Code Structure/Design/Resume

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,32 +2,37 @@ function init(){
     
     // On Doc ready -> set section heights to the viewport heights
     vph = $(window).height();
-    $('#homepg').css('height',vph);
+    $('#homepage').css('height', vph);
     /* affix the navbar after scrolling down */
     $(".navbar").affix({offset: {top: 10} });
+}
+
+function setScrollingEvent(){
+    //Set Event Handling for navbar
+    $(".nav_btn").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#homepage").offset().top
+        }, 2000);
+    });
 }
 
 $(document).ready(function(){
     init();
 })
- 
-// on Resize -> set section height/width to viewport
-// $(window).resize(function(){
-//     vpw = $(window).width();
-//     vph = $(window).height();
 
-//     $('section').css('height',vph);
-// });
+
+
+
 
 
 
 //Find largest height of boxed item -> set all boxes to that height
-var maxHeight = 0;
+// var maxHeight = 0;
 
-$(".skill_item").each(function(){
-    if( $(this).height() > maxHeight){
-        maxHeight = $(this).height();
-    }
-});
+// $(".skill_item").each(function(){
+//     if( $(this).height() > maxHeight){
+//         maxHeight = $(this).height();
+//     }
+// });
 
-$(".skill_item").height(maxHeight);
+// $(".skill_item").height(maxHeight);
