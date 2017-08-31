0acad4e0714e8fa3a0b9fba72825e42f7127b2a9
Jason Hannanto <jason.hannanto@gmail.com>
2017-08-30 17:27:31 -0700

    Add Navbar functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,34 @@
+function setScrollingEvent(){
+    //Set Event Handling for navbar
+    $('.home_btn').click(function(){
+        $('#homepage').animatescroll({padding:50});
+    })
+
+    $('.about_btn').click(function(){
+        $('#aboutpage').animatescroll({padding:50});
+    })
+
+    $('.skills_btn').click(function(){
+        $('#skillspage').animatescroll({padding:50});
+    })
+
+    $('.resume_btn').click(function(){
+        $('#resumepage').animatescroll({padding:49});
+    })
+
+    // $('project_btn').click(function(){
+    //     $('#aboutpage').animatescroll({padding:50});
+    // })
+
+    $('.contact_btn').click(function(){
+        $('#contactpage').animatescroll({padding:50});
+    })
+
+    $('#logo-brand').click(function(){
+        $('#homepage').animatescroll();
+    })
+}   
+
 function init(){
     
     // On Doc ready -> set section heights to the viewport heights
@@ -5,34 +36,10 @@ function init(){
     $('#homepage').css('height', vph);
     /* affix the navbar after scrolling down */
     $(".navbar").affix({offset: {top: 10} });
-}
 
-function setScrollingEvent(){
-    //Set Event Handling for navbar
-    $(".nav_btn").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#homepage").offset().top
-        }, 2000);
-    });
+    setScrollingEvent();
 }
 
 $(document).ready(function(){
     init();
 })
-
-
-
-
-
-
-
-//Find largest height of boxed item -> set all boxes to that height
-// var maxHeight = 0;
-
-// $(".skill_item").each(function(){
-//     if( $(this).height() > maxHeight){
-//         maxHeight = $(this).height();
-//     }
-// });
-
-// $(".skill_item").height(maxHeight);
